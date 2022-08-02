c650dedb970cd6b34b3b9d53ee35a1b11445e2af
Maribel <lwang79@zoho.com>
2022-08-01 17:19:06 -0700

    baseline established

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <text>Welcome to the hentai prompt generator!</text>
+      <BrowserRouter>
+        <>
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+          </Routes>
+        </>
+      </BrowserRouter>
+
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+    return (
+        <text>hello again!</text>
+    );
+}
+
+export default Home;
