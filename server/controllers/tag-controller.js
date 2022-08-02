fffb91a41c292383fd7e184a5799bd0dd4eb8fcb
Maribel <lwang79@zoho.com>
2022-08-02 09:33:32 -0700

    new branch for server

---
+++ server/controllers/tag-controller.js
@@ -0,0 +1,14 @@
+const { Tag } = require('../models');
+
+module.exports = {
+    // get a single tag by id or tagTitle
+
+    // get all tags
+
+    // create a tag
+
+    // update a tag by id or tagTitle
+
+    // delete a tag by id or tagTitle
+
+}
