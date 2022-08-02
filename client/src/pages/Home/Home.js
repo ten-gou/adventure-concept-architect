8e2108695b728172895d35bc063fa3688367b52c
Maribel <lwang79@zoho.com>
2022-08-01 22:02:54 -0700

    setting up base page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
+import {
+    CssBaseline,
+    Box,
+    Grid,
+    Stack
+} from '@mui/material';
+import { styled  } from '@mui/material/styles';
+
+const BoxBackground = styled(Box)(({ theme }) => ({
+    paddingTop: '2rem',
+    paddingBottom: '3rem',
+    background: 'rgb(240, 240, 255)'
+}));
+
 const Home = () => {
     return (
+        <BoxBackground>
+        <CssBaseline />
         <text>hello again!</text>
+        </BoxBackground>
     );
 }
 
