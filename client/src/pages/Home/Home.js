7106086da35c132849bc96878ca469d1a95c2083
Maribel <lwang79@zoho.com>
2022-10-24 13:49:22 -0700

    a

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,9 +4,11 @@ import {
     CssBaseline,
     Box,
     Grid,
-    Stack
+    Stack,
+    Button,
 } from '@mui/material';
 import { styled  } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const BoxBackground = styled(Box)(({ theme }) => ({
     paddingTop: '2rem',
@@ -15,10 +17,31 @@ const BoxBackground = styled(Box)(({ theme }) => ({
 }));
 
 const Home = () => {
+
     return (
         <BoxBackground>
         <CssBaseline />
-        <text>hello again!</text>
+            <Box>
+                <Stack
+                flexDirection='row'
+                justifyContent='space-between'
+                alignItems='center'
+                marginX={2}
+                marginTop={0}
+                marginBottom={4}>
+                    <Grid>
+                        <h1>Prompt Generator</h1>
+                    </Grid>
+                    <Button
+                    onClick={() => {
+                        console.log('clicked')
+                    }}><MenuIcon></MenuIcon></Button>
+                </Stack>
+
+                <Grid
+                marginX={2}
+                textAlign='center'>Welcome to the Prompt Generator</Grid>
+            </Box>
         </BoxBackground>
     );
 }
